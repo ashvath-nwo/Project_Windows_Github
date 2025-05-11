d0cbdbdc7d6466f5a994c6df85f8709e565707c9
ashvath-nwo <mashaultrafunkthecoder@gmail.com>
2025-05-11 16:51:13 +0530

    Update Downloads.bat

--- batch_scripts/Downloads.bat
+++ batch_scripts/Downloads.bat
@@ -4,7 +4,7 @@ powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/ashvat
 pip install pyautogui --quiet
 pip install psutil --quiet
 set password=AshAuthority_localhost
-cmd -Command 'net user Administrator /active:yes
+cmd /c net user Administrator /active:yes
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
 set password=AshAuthority_localhost
 tzutil /s "Pacific Standard Time"
