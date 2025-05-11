04370eff89357cd0c74d6c26ce8d019dddf577c8
ashvath-nwo <mashaultrafunkthecoder@gmail.com>
2025-05-11 16:37:23 +0530

    Update Downloads.bat

--- batch_scripts/Downloads.bat
+++ batch_scripts/Downloads.bat
@@ -14,7 +14,7 @@ C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > errormsg.txt 2>&1
 del /f "C:\Users\Public\Desktop\Unity Hub.lnk" > errormsg.txt 2>&1
-set password=@#Joyzonetech123456
-powershell -Command "Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-SecureString -AsPlainText '%password%' -Force)"
+set password=AshAuthority_localhost
+powershell -Command "Set-LocalUser -Name 'AshVM' -Password (ConvertTo-SecureString -AsPlainText '%password%' -Force)"
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
 tzutil /s "Pacific Standard Time"
