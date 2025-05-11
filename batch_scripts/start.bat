2e51e44fde3ac4365aa0b8560cd959c3a2d3c9af
ashvath-nwo <mashaultrafunkthecoder@gmail.com>
2025-05-11 16:36:22 +0530

    Update start.bat

--- batch_scripts/start.bat
+++ batch_scripts/start.bat
@@ -27,13 +27,3 @@ echo Go follow me on GitHub. bit.ly/AshGithub :)
 echo the following Operating System is Windows Server 2025(server version of Windows 11)
 echo AnyDesk ID is: %ID%
 echo AnyDesk Password: AshAuthority_localhost
-._^|__  /___.._.__...__^|_   _^|__..___^| ^|__...  
-echo  _  ^| ^|/ _ \^| ^| ^| ^| / // _ \^| '_ \ / _ \^| ^|/ _ \/ __^| '_ \ 
-echo ^| ^|_^| ^| ^|_^| ^| ^|_^| ^|/ /^| ^|_^| ^| ^| ^| ^|  __/^| ^|  __/ /__^| ^| ^| ^|
-echo  \___/ \___/ \__, /____\___/^|_^| ^|_^|\___^|^|_^|\___^|\___^|_^| ^|_^|
-echo .............^|___/..........................................
-echo ............................................................
-echo ............................................................
-echo AnyDesk ID is: %ID%
-echo AnyDesk Password: joyzonetech
-echo You can login now!
