a540a0d5fd72e9a061b31ca9ab7a3a46fe048e9a
Ashvath <mashaultrafunkthecoder@gmail.com>
2025-12-07 11:45:03 +0530

    Update Downloads.bat

--- batch_scripts/Downloads.bat
+++ batch_scripts/Downloads.bat
@@ -8,5 +8,5 @@ cmd /c net localgroup
 cmd /c net user AshVM /add
 cmd /c net localgroup Admimnistrators AshVM /add
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
-set password=AshAuthority_localhost
-tzutil /s "Pacific Standard Time"
+set password=AshAuthority
+tzutil /s "India Standard Time"
